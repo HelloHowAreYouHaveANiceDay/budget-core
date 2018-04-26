ab9b55761def3eeffc54776a694140d362fe9ae8
HelloHowAreYouHaveANiceDay <liboli.lilibo@gmail.com>
2018-04-26 11:07:44 -0400

    needs refactoring, reading from configuration file working.

--- index.js
+++ index.js
@@ -10,17 +10,39 @@ const sources = configuration.sources;
 const firstFolder = sources[2];
 
 const readCsv = (fileName) => {
-  const file = folder.readFile(`${rootPath}\\${firstFolder.folderName}\\${fileName}`);
+  const file = folder.readFile(fileName);
   file.then((result)=>{
    // console.log(result);
-    console.log(csv.parse(result).length);
+    console.log(csv.parse(result));
   }).catch((err) => {
     throw err;
   })
 }
 
+const getFolderPath = source => source.path;
+const prependRoot = rootPath => childPath => rootPath + childPath;
+const appendFilename = directoryPath => filePath => directoryPath + '\\' + filePath;
+
+const getSourceFolder = R.pipe(getFolderPath, prependRoot(rootPath));
+
+const readSourceFolder = R.pipe(getSourceFolder, folder.readDir);
+
 // directory.then((result)=>{
 //   R.forEach(readCsv, result);
 // })
 
-console.log(configuration.types);
+// console.log(configuration.types);
+R.forEach((source)=>{
+  readSourceFolder(source).then((result) => {
+    R.forEach((file)=>{
+      const folderPath = getSourceFolder(source);
+      const filePath = appendFilename(folderPath)(file)
+      readCsv(filePath).then((d)=>{
+        console.log(d);
+      })
+    }, result)
+  })
+}, sources);
+
+
+
