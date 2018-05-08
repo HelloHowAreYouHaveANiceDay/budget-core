7df5873499fbb63224c84585b8a37336350154e1
Libo Li <libo.li@wework.com>
2018-05-08 14:26:01 -0400

    added mock json data. json verifcation and parsing

--- json.js
+++ json.js
@@ -1,11 +1,16 @@
 const R = require('ramda');
 
+const H = require('./helper');
+
+const req = require;
+
 /**
  * getKeys from JSON file
  * @param {string} filepath
  *
  * @returns {array} keys from json objects
  */
+module.exports.getKeys = R.pipe(require, H.getKeys);
 
 /**
  * isValid checks whether JSON contails a valid collection
@@ -13,6 +18,7 @@ const R = require('ramda');
  *
  * @returns {bool} whether file is a valid collection
  */
+module.exports.isValid = R.pipe(R.values, R.is(Array));
 
 /**
  * toCsv converts a JSON collection to a csv
@@ -20,3 +26,24 @@ const R = require('ramda');
  *
  * @returns {string} csv as string
  */
+
+/**
+ * getDepth returns depth from JSON file
+ * @param {string} filepath
+ * 
+ * @returns {int} depth 
+ */
+// const getDepth = filepath => {
+//   const file = require(filepath);
+//   let depth = 0
+//   if (file.chidren) {
+//     file.children.forEach((d) => {
+//       const tempDepth = getDepth(d)
+//       if (tempDepth > depth) {
+//         depth = tempDepth;
+//       }
+//     })
+//   }
+//   return 1 + depth
+// }
+// module.exports.getDepth = getDepth;
--- tests/json.test.js
+++ tests/json.test.js
@@ -1,5 +1,27 @@
-const JSON = require('../json');
+const path = require('path');
+const J = require('../json');
 
 test('getKeys gets Keys from JSON file', () => {
-
+  const filepath = path.join(__dirname, '../example_data/f_players_mock.json');
+  const test1 = J.getKeys(filepath);
+  expect(test1)
+    .toBeInstanceOf(Array);
+  expect(test1.length)
+    .toEqual(7827);
 });
+
+test('isValid checks whether JSON file contains a valid collection', () => {
+  const filepath = path.join(__dirname, '../example_data/f_players_mock.json');
+  const testOb = require(filepath);
+  const test1 = J.isValid(filepath);
+  expect(test1)
+    .toEqual(true);
+});
+
+// test('getDepth gets depth from JSON file', () => {
+//   const filepath = path.join(__dirname, '../example_data/f_game_mock.json');
+//   const test1 = J.getDepth(filepath);
+//   console.log(test1);
+//   expect(test1)
+//     .toBeInstanceOf(Number)
+// });
