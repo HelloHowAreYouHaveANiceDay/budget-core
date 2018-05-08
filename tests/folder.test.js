6aa4aac30596e68d7be53fe4f6dfad3a8b7175e0
HelloHowAreYouHaveANiceDay <liboli.lilibo@gmail.com>
2018-05-07 23:03:30 -0400

    add

--- tests/folder.test.js
+++ tests/folder.test.js
@@ -1,13 +1,13 @@
-const folder = require("../folder");
+const folder = require('../folder');
 
-test("readDirectory gets file names", () => {
+test('readDirectory gets file names', () => {
   expect.assertions(1);
-  return folder.readDir("./example_data").then(data => {
+  return folder.readDir('./example_data').then((data) => {
     expect(data).toEqual([
-      "amazon_mock.csv",
-      "chase_acct_mock.csv",
-      "chase_cc_mock.csv",
-      "mint_mock.csv"
+      'amazon_mock.csv',
+      'chase_acct_mock.csv',
+      'chase_cc_mock.csv',
+      'mint_mock.csv',
     ]);
   });
 });
